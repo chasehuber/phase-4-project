d67581f01edc45a7d218a323f85b6ad0ca62ed39
Yeeun Jung <rjyeeun@gmail.com>
2023-03-09 22:44:17 -0500

    delete post feature done

--- client/src/components/PostDetails.js
+++ client/src/components/PostDetails.js
@@ -1,9 +1,26 @@
 import React, {useEffect} from "react";
+import {useHistory} from "react-router-dom";
 import NewReplyForm from "./NewReplyForm"
 import ReplyCard from "./ReplyCard";
 
-function PostDetails({posts, handleNewReply , currentPost, currentUser, setReplies, statereplies, setCurrentPost}) {
+function PostDetails({posts, handleNewReply , currentPost, currentUser, setReplies, statereplies, setCurrentPost, onDeletePost}) {
     const {id, title, body, breed, creator_id, replies} = currentPost
+    const history = useHistory()
+    const handleDelete = () => {
+        if (currentPost.creator_id === currentUser.id) {
+        console.log("currentUser id: ", currentUser.id);
+        console.log("currentPost id: ", currentPost.id);
+        
+        fetch(`/posts/${id}`,
+        { method: 'DELETE'})
+        .then(() => onDeletePost(id))
+
+        history.push('/posts')
+    }
+    else {
+        alert("You cannot delete a post that you didn't post!")
+    }
+}
 
     //Fetching So That currentPost Is Updated As New Replies Are Submitted
     useEffect(() => {
@@ -19,14 +36,17 @@ function PostDetails({posts, handleNewReply , currentPost, currentUser, setRepli
         />
     ))
 
+
+
     return (
         <>
-        <h1>hi post deets</h1>
+        <h1>{currentPost.creator_id}</h1>
         <div>
             <p>{title}</p>
             <p>{breed}</p>
             <p>{body}</p>
             <div>{replyArray}</div>
+            <button onClick={handleDelete}>Delete Post</button>
         </div>
         <NewReplyForm
             replies={statereplies} 
